934f27ced34a801a3f9165a6f97524e616b6c726
Jeremy Cui <tsuijy@gmail.com>
2010-10-15 23:39:45 +0800

    Fixed the build file- handle the escape character

--- script/teamcity.rb
+++ script/teamcity.rb
@@ -3,7 +3,7 @@ require  "albacore"
 task :default => [:build]
 
 msbuild :build do |msb|
-  msb.solution = "src\AgileWizard.sln"
+  msb.solution = "src\\AgileWizard.sln"
   msb.targets :clean, :build
   msb.properties :configuration => :release
 end
