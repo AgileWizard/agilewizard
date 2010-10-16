cf5042624e1f0348fb2ecdf1ae8baa8418c13847
Jeremy Cui <tsuijy@gmail.com>
2010-10-16 14:45:44 +0800

    Added test project for teamcity code coverage test

---
+++ src/AgileWizard.Domain.Tests/IntegrateToTeamctiyTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit.Sdk;
+using Xunit;
+
+namespace AgileWizard.Domain.Tests
+{
+    public class IntegrateToTeamctiyTest
+    {
+        [Fact]
+        public void Add_TwoSimleNumber_ReturnSummaryOfThem()
+        {
+            var a = 5555;
+            var b = 3333;
+            var s = 8888;
+
+            var t = new IntegrateToTeamctiy();
+
+            Assert.Equal(s, t.Add(a, b));
+        }
+
+        [Fact]
+        public void GetTypeName_NullValuePassedIn_ThrowNullReferenceException()
+        {
+            var t = new IntegrateToTeamctiy();
+
+            Assert.Throws<NullReferenceException>(() => t.GetTypeName<string>(default(string)));
+        }
+
+        [Fact]
+        public void GetFirstPropertyValue_JustForCodeCoverageTest()
+        {
+            var t = new IntegrateToTeamctiy();
+
+            var testObject = "agile";
+
+            var propetyValue = t.GetPropertyValue<string>(testObject, "length");
+            Assert.NotSame(5, propetyValue);
+            Assert.True(int.Parse(propetyValue.ToString()) == 5);
+        }
+    }
+}
---
+++ src/AgileWizard.Domain/IntegrateToTeamctiy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AgileWizard.Domain
+{
+    public class IntegrateToTeamctiy
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public string GetTypeName<T>(T obj)
+        {
+            return obj.GetType().Name;
+        }
+
+        public object GetPropertyValue<T>(T obj, string propName)
+        {
+            if (obj == null) return 0;
+
+            var type = typeof(T);
+            var props = type.GetProperties();
+            var firstPropValue = default(Object);
+
+            if (props != null && props.Length > 0)
+            {
+                var selectProp = props.Single(p => string.Compare(p.Name, propName, StringComparison.OrdinalIgnoreCase) == 0);
+                if (selectProp.CanRead) firstPropValue = selectProp.GetValue(obj, null);
+            }
+
+            return firstPropValue;
+        }
+    }
+}
