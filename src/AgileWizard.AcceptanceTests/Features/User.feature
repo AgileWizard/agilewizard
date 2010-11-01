b24b5be58bdb6294372d00ac9268857a36a5500d
Steven@3500 <jdomzhang@gmail.com>
2010-11-01 17:59:33 +0800

    Added AcceptanceTests project and User feature file

---
+++ src/AgileWizard.AcceptanceTests/Features/User.feature
@@ -0,0 +1,11 @@
+﻿Feature: User
+	In order to indentify user
+	As a website master
+	I want user to login with username and password
+
+@ignore
+Scenario: Login
+	Given I open login page
+	And I enter username - 'agilewizard' and password - 'agilewizard'
+	When I press login
+	Then I should be redirected to main page
---
+++ src/AgileWizard.AcceptanceTests/Steps/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace AgileWizard.AcceptanceTests.Steps {
+    [Binding]
+    public class User {
+        [Given(@"I open login page")]
+        public void GivenIOpenLoginPage() {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I enter username - 'agilewizard' and password - 'agilewizard'")]
+        public void GivenIEnterUsername_AgilewizardAndPassword_Agilewizard() {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I press login")]
+        public void WhenIPressLogin() {
+            ScenarioContext.Current.Pending();
+        }
+
+
+        [Then(@"I should be redirected to main page")]
+        public void ThenIShouldBeRedirectedToMainPage() {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
