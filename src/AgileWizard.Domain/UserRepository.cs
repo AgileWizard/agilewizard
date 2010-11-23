93ab89eab9cb2456894cfc03d28c2d132c03b83c
Daniel Teng <tengzhenyu@gmail.com>
2010-11-23 16:04:59 +0800

    make user repository more readable

--- src/AgileWizard.Domain/UserRepository.cs
+++ src/AgileWizard.Domain/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using AgileWizard.Domain.QueryIndex;
 using Raven.Client;
 
@@ -7,6 +8,7 @@ namespace AgileWizard.Domain
     public class UserRepository
     {
         private readonly IDocumentSession _session;
+        private IEnumerable<User> _resultUsers;
 
         public UserRepository(IDocumentSession session)
         {
@@ -15,15 +17,28 @@ namespace AgileWizard.Domain
 
         public User GetUserByName(string userName)
         {
+            GetUsersByName(userName);
+
+            return ThereIsMatchingUser() ? FirstUser() : User.EmptyUser();
+        }
+
+        private void GetUsersByName(string userName)
+        {
             var users = _session.Query<User>(typeof(UserIndexByUserName).Name);
 
-            var resultUsers = from x in users
-                              where x.UserName == userName
-                              select x;
+            _resultUsers = from x in users
+                   where x.UserName == userName
+                   select x;
+        }
+        
+        private bool ThereIsMatchingUser()
+        {
+            return _resultUsers.Count() > 0;
+        }
 
-            if (resultUsers.Count() > 0)
-                return resultUsers.First();
-            return User.EmptyUser();
+        private User FirstUser()
+        {
+            return _resultUsers.First();
         }
     }
 }
