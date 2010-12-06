811f2b71843b2586d5d816abcc9f90144244a70c
Mike Li <lidingshan@gmail.com>
2010-12-06 22:49:06 +0800

    Create AgileWizard.Locale assembly and its tests

---
+++ src/AgileWizard.Locale.Tests/LocaleHelperTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace AgileWizard.Locale.Tests
+{
+    public class LocaleHelperTest
+    {
+        [Fact]
+        public void choose_US_should_display_username_label_in_English()
+        {
+            string localeName = "en-US";
+            string stringId = "UI_UserName";
+
+            string expectedLabelName = "User Name";
+            string actualLabelName = LocaleHelper.GetLocaleString(stringId, localeName);
+            Assert.Equal(expectedLabelName, actualLabelName);
+        }
+
+        [Fact]
+        public void choose_China_should_display_username_label_in_Chinese()
+        {
+            string localeName = "zh-CN";
+            string stringId = "UI_UserName";
+
+            string expectedLabelName = "用户名";
+            string actualLabelName = LocaleHelper.GetLocaleString(stringId, localeName);
+            Assert.Equal(expectedLabelName, actualLabelName);
+        }
+    }
+}
---
+++ src/AgileWizard.Locale/LocaleHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Resources;
+using System.Globalization;
+
+namespace AgileWizard.Locale
+{
+    public static class LocaleHelper
+    {
+        static ResourceManager localeResourceManager = new ResourceManager("AgileWizard.Locale.String", System.Reflection.Assembly.GetCallingAssembly());
+
+        public static string GetLocaleString(string stringId, string localeName)
+        {
+            CultureInfo cultureInfo = new CultureInfo(localeName);
+            return localeResourceManager.GetString(stringId, cultureInfo);
+        }       
+    }
+}
