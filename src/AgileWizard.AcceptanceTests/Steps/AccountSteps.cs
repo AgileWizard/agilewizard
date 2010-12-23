e1bf89900036d5c0e3deba209d7f996429c751e2
Steven Zhang <jdomzhang@gmail.com>
2010-12-23 20:58:37 +0800

    Renamed classes under steps folder to end with steps

---
+++ src/AgileWizard.AcceptanceTests/Steps/AccountSteps.cs
@@ -0,0 +1,38 @@
+﻿using AgileWizard.AcceptanceTests.Helper;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace AgileWizard.AcceptanceTests.Steps
+{
+    [Binding]
+    public class AccountSteps
+    {
+        [Given(@"open login page")]
+        public void GivenOpenLoginPage()
+        {
+            BrowserHelper.OpenPage("Account/LogOn");
+        }
+
+        [Given(@"enter username - '(\w+)' and password - '(\w+)'")]
+        public void GivenEnterUsernameAndPassword(string userName, string password)
+        {
+            BrowserHelper.InputText("UserName", userName);
+            BrowserHelper.InputText("Password", password);
+        }
+
+        [When(@"press login button")]
+        public void WhenPressLoginButton()
+        {
+            BrowserHelper.PressSubmitButton();
+        }
+
+        [Then(@"should be redirected to main page")]
+        public void ThenShouldBeRedirectedToMainPage()
+        {
+            var browser = BrowserHelper.Browser;
+
+            Assert.Equal(BrowserHelper.WebsiteUrl, browser.Url);
+        }
+
+    }
+}
