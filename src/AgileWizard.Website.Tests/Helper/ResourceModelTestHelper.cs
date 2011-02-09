99848002ed6b2b2822d84a0cc70baa871a258230
Daniel Teng <tengzhenyu@gmail.com>
2011-02-09 23:12:34 +0800

    add helper classes, fix the build

---
+++ src/AgileWizard.Website.Tests/Helper/ResourceModelTestHelper.cs
@@ -0,0 +1,13 @@
+﻿using AgileWizard.Domain.Models;
+using AgileWizard.Website.Models;
+
+namespace AgileWizard.Website.Tests.Helper
+{
+    public class ResourceModelTestHelper
+    {
+        public static ResourceDetailViewModel BuildDefaultResourceDetailViewModel()
+        {
+            return new ResourceDetailViewModel(Resource.DefaultResource());
+        }
+    }
+}
