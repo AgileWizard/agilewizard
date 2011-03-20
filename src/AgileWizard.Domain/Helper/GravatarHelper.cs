f6c72c38d401f99d00665a9aa3b1547ab14dba0e
Jackson Zhang <zbcjackson@gmail.com>
2011-03-20 17:56:33 +0800

    Implement gravatar helper

---
+++ src/AgileWizard.Domain/Helper/GravatarHelper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using StructureMap;
+using StructureMap.Attributes;
+using StructureMap.Pipeline;
+
+namespace AgileWizard.Domain.Helper
+{
+    public class GravatarHelper
+    {
+        private string _email;
+        [SetterProperty]
+        public IHash Hash { get; set; }
+
+        public GravatarHelper(string email)
+        {
+            this._email = email;
+        }
+
+        private string HashCode
+        {
+            get { return this.Hash.MD5(_email.ToLower()); }
+        }
+
+        public string Url
+        {
+            get
+            {
+                return string.Format("http://gravatar.com/avatar/{0}?d=mm", HashCode);
+            }
+        }
+
+        public static string AvatarUrl(string email)
+        {
+            var args = new ExplicitArguments(new Dictionary<string, object>{{"email", email}});
+            var gravatar = ObjectFactory.GetInstance<GravatarHelper>(args);
+            return gravatar.Url;
+        }
+    }
+}
