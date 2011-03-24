fdde11708466c8270dc30240f18825dfb79db45d
Jeremy Cui <tsuijy@gmail.com>
2011-03-24 21:51:25 +0800

    removed unused usings

--- src/AgileWizard.Domain.Tests/Expression/TopLatestResourceListQueryExperssion_ResourceListBuilderTest.cs
+++ src/AgileWizard.Domain.Tests/Expression/TopLatestResourceListQueryExperssion_ResourceListBuilderTest.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using AgileWizard.Domain.Expression;
 using AgileWizard.Domain.Models;
 using Xunit;
